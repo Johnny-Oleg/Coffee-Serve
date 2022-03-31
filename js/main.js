01b3ce97964c8978a53a738e9c6c1f6359630654
Johnny-Oleg <neojin.entity@gmail.com>
2022-03-31 19:17:53 +0300

    slider and footer completed

--- js/main.js
+++ js/main.js
@@ -3,4 +3,43 @@ $(function () {
         $('.❓📦🔖').removeClass('⚠️');
         $(this).parent().addClass('⚠️');
     })
+
+	$('.🔂📦').slick({
+        dots: true,
+		arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        cssEase: 'linear',
+        // responsive: [
+        //     {
+        //         breakpoint: 891,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             arrows: false,
+        //             dots: true,
+        //         },
+        //         breakpoint: 461,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             arrows: false,
+        //             dots: true,
+        //         },
+        //         breakpoint: 361,
+        //         settings: {
+        //             variableWidth: false,
+        //             slidesToShow: 1,
+        //         },
+        //     },
+        // ],
+    });
+
+	// $('.nav__btn').on('click', function () {
+    //     $('.nav__btn').toggleClass('active');
+	// })
 })
